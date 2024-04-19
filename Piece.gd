98da211200a8d916a35041c7b14aee9df4f86de6
dhammar <davehammarstedt@gmail.com>
2024-04-19 14:24:49 -0700

    add king piece sprites and logic for displaying

--- Piece.gd
+++ Piece.gd
@@ -3,6 +3,10 @@ extends Node2D
 # "red" or "black" or player?
 @export_enum("red", "black") var color: String
 @export var selected: bool
+@export var king: bool
+
+var sprite;
+var selected_sprite;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,23 +17,29 @@ func create_piece(col, pos):
 	position = pos
 	print("adding %s piece to %s" % [color, pos])
 	if color == "black":
-		$BlackIdle.show();
+		sprite = $BlackIdle;
+		selected_sprite = $BlackSelected;
 	else:
-		$RedIdle.show();
+		sprite = $RedIdle;
+		selected_sprite = $RedSelected;
+	sprite.show()
 
 func select():
-	if color == "black":
-		if selected:
-			$BlackIdle.show();
-			$BlackSelected.hide();
-		else: 
-			$BlackIdle.hide();
-			$BlackSelected.show();
-	else:
-		if selected:
-			$RedIdle.show();
-			$RedSelected.hide();
-		else: 
-			$RedIdle.hide();
-			$RedSelected.show();
 	selected = !selected
+	if selected:
+		sprite.hide()
+		selected_sprite.show()
+	else:
+		selected_sprite.hide()
+		sprite.show()
+
+func set_king():
+	king = true
+	sprite.hide()
+	if color == "black":
+		sprite = $BlackKingIdle
+		selected_sprite = $BlackKingSelected
+	else:
+		sprite = $RedKingIdle
+		selected_sprite = $RedKingSelected
+	sprite.show()
